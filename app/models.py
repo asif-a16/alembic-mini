4953bc2b8b6610aecdb0c1b33a181eaeb84fd93e
Asif <asif.16f@gmail.com>
2026-05-25 20:07:37 +0100

    Apply add user name migration

--- app/models.py
+++ app/models.py
@@ -13,3 +13,4 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(320), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(100), nullable=False, server_default="")
